1f4988ef1043cb7bb4022ffa66bce1f2b73957a4
lashkoag <lashko-ag@energos.su>
2021-03-31 11:25:31 +0600

    event queue into one thread

--- src/main.cpp
+++ src/main.cpp
@@ -9,24 +9,38 @@
 #include "mbed_events.h"
 #include <stdio.h>
 
+// #define FLAG_CANCEL_TIMESIGNAL (1U << 0)
+// EventFlags eventFlags;
+
 DigitalOut led(PB_0);
 
 // creates a queue with the default size
 EventQueue queue;
 
 EventQueue queue2;
-
+EventQueue queue3;
+EventQueue queue4;
 Thread thread;
 Thread thread2;  
+Thread thread3; 
+
+int32_t uniqueID = 0;
+bool flag_cancel_timeSignal = 0;
+bool flag_turnOFF_led = 0;
 
 void led_inverse()
 {
+  // printf("========== led_inverse() ==========================\n");
     led = !led;
 }
 
 void counting(){
+  if(flag_cancel_timeSignal){return;}
   printf("---------------  counting()  ----------------------\n");
   for(uint32_t i = 0; i<=4000000000; ++i){
+    if(flag_cancel_timeSignal){
+      break;
+    }
     if(i== 10){
       printf("10\n");
     }
@@ -44,30 +58,95 @@ void counting(){
 
 
 void for_th(){
-  ThisThread::get_id();
-  queue.call(printf, "thread counting ThisThread::get_id() = %p\n", ThisThread::get_id());
-  queue.call(counting);
-  queue.dispatch();
+  flag_cancel_timeSignal=0;
+  // while(1){
+    
+    // ThisThread::get_id();
+    // queue.call(printf, "thread counting ThisThread::get_id() = %p\n", ThisThread::get_id());
+    uniqueID = queue.call(counting);
+    // queue.call(printf, "uniqueID = queue.call(counting); = %d\n", uniqueID);
+  // }
+
 }
+// void for_blink(){
+//   led=1;
+  // printf("========== for_blink() ==========================\n");
+  // queue3.call_every(50, led_inverse);
+  // if(flag_turnOFF_led){
+  //   queue3.break_dispatch();
+  // }
+  // queue3.dispatch();
+// }
 
 void for_th2(){
-  queue2.call(printf, "thread led_inverse ThisThread::get_id() = %p\n", ThisThread::get_id());
-  queue2.call_every(50, led_inverse);
-  queue2.dispatch();
+  int32_t count=0;
+  while(1){
+    count++;
+    ThisThread::sleep_for(2000);
+    flag_cancel_timeSignal = true;
+    queue.call(printf, "count = %d\n", count+=10);
+    queue.call_every(50, led_inverse);
+    
+    // bool result_cancel = queue.cancel(uniqueID);
+    // queue.call(printf, "result_cancel = %d\n", result_cancel);
+
+    queue.call(printf, "thread led_inverse ThisThread::get_id() = %p\n", ThisThread::get_id());
+  
+    // queue.call(printf, "try cancel = %d\n", uniqueID);
+  }
+
+  // queue.call_every(50, led_inverse);
 }
 
+void swit(){
+  
+  int value = 0;
+
+  value = 100;
+    while(1){
+      switch (value)
+      {
+      case 100:
+        uniqueID = queue4.call(counting);
+        break;
+
+      case 200:
+        flag_cancel_timeSignal = true;
+        int countt = 0;
+        queue4.call(printf, "countt = %d\n", countt+=10);
+        break;
+
+      // case 300:
+      //   int counttt = 0;
+      //   queue4.call(printf, "counttt = %d\n", counttt+=1000);
+      //   break;
+
+      // default:
+      //   break;
+      }
+      ThisThread::sleep_for(5000);
+      printf( "hello arm\n");
+
+      value =200;
+  }
+}
 
 int main() {
 
   printf("====================================\n");
 
-  thread.start(for_th);
+  // thread.start(for_th);
 
-  thread2.start(for_th2);
+  // thread2.start(for_th2);
+  // thread3.start(for_blink);
 
+  thread3.start(swit);
+
+  // queue.dispatch();
+  queue4.dispatch();
   printf("===vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv====\n");
 
-    while(1){
+    while(1){ 
 
     }
 }
