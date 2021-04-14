e5b952377cb1376b8b7a2752b79413e9db0c4e96
lashkoag <lashko-ag@energos.su>
2021-04-14 11:15:31 +0600

    for massive. Slave must have format(8,3), but does not have frequency. MOST IMPORTANT information: connect GND(slave) <-> GND(master)

--- src/main.cpp
+++ src/main.cpp
@@ -30,10 +30,9 @@ SPISlave device(PA_7, PA_6, PA_5, PA_4);
 // SPI_MISO    = PA_6,
 // SPI_SCK     = PA_5,
 // SPI_CS      = PB_6,
-DigitalOut drdyOut(PC_7);
 
+DigitalOut drdyOut(PC_7);
 DigitalOut nss(PA_4);
-uint32_t byte_send = 0;
 
 int main()
 {
@@ -43,11 +42,13 @@ int main()
     // device.frequency(1000000); // 1MHz
 
     uint8_t *BufferMsv = new uint8_t[3];
+
     BufferMsv[0] = 0x6F; //111
     BufferMsv[1] = 0x64; //100
     BufferMsv[2] = 0x32; //50
 
     int v;
+
     while (1)
     {
         drdyOut = 1;
@@ -56,49 +57,47 @@ int main()
         {
             wait_us(1);
             v = device.read(); // Read byte from master
-            printf("v register = 0x%X\n\r", v);
+                printf("v register = 0x%X\n\r", v);
             nss = 0;
-            device.reply(v);
+            device.reply(BufferMsv[0]); // 0x6F 111
+            device.reply(BufferMsv[1]); //0x64 100
+            device.reply(BufferMsv[2]); //0x32 50
+
             nss = 1;
-
-            drdyOut = 0;
         }
-
-        if (BufferMsv != NULL)
-        {
-            delete[] BufferMsv;
-        }
+        drdyOut = 0;
     }
 
-    /*code for master*/
-    //H743ZI
-    SPI spi(PD_7, PA_6, PA_5, PA_4); // mosi, miso, sclk, ssel
-    DigitalOut chipSelect(PA_4);
-    InterruptIn drdyIN(PE_3);
-
-    chipSelect = 1;
-    spi.format(8, 3);
-    spi.frequency(1000000); //1MHz
-
-    while (1)
+    if (BufferMsv != NULL)
     {
-        chipSelect = 0;
-
-        // Send 0xAA, the command to read the WHOAMI register
-        int whoami1 = spi.write(0xAA); //get answer 1 byte from slave
-            printf("WHOAMI register = 0x%X\n\r", whoami1); 
-        // Deselect the device
-        chipSelect = 1;
-
-        ThisThread::sleep_for(5000);
-
-        chipSelect = 0;
-        //Send 0xAA, the command to read the WHOAMI register
-        whoami1 = spi.write(0xCC);
-            printf("WHOAMI register = 0x%X\n\r", whoami1);
-
-        //Deselect the device
-        chipSelect = 1;
-
-        ThisThread::sleep_for(5000);
+        delete[] BufferMsv;
     }
+}
+
+/*code for master*/
+//H743ZI
+SPI spi(PD_7, PA_6, PA_5, PA_4); // mosi, miso, sclk, ssel
+DigitalOut chipSelect(PA_4);
+InterruptIn drdyIN(PE_3);
+
+chipSelect = 1;
+spi.format(8, 3);
+spi.frequency(1000000); //1MHz
+
+while (1)
+{
+    chipSelect = 0;
+    spi.write(txBufferMsv, 3, rxBufferMsv, 3);
+
+    // Deselect the device
+    chipSelect = 1;
+
+        printf("===============================\n");
+    for (uint8_t i = 0; i < 3; ++i)
+    {
+        printf("rxBufferMsv[%d] = %d \n", i, rxBufferMsv[i]);
+    }
+        printf("===============================\n");
+
+    ThisThread::sleep_for(5000);
+}
